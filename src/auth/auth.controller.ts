0c04cd22b83aca3a6917caa2af96c53d7bf4762f
Matt Eppy <102959719+Mattjeppy@users.noreply.github.com>
2025-04-14 15:50:02 +0100

    link up controller and service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,11 +7,11 @@ export class AuthController {
 
   @Post('signup')
   signup() {
-    return 'I have signed up';
+    return this.authService.signup();
   }
 
   @Post('signin')
   signin() {
-    return 'I have signed in';
+    return this.authService.signin();
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable({})
 export class AuthService {
+  signup() {
+    return { msg: 'I have signed up' };
+  }
 
-login() {}
-
-signup() {}
-
+  signin() {
+    return { msg: 'I have signed in' };
+  }
 }

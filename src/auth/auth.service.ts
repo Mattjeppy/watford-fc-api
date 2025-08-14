843ae37e6f05ebc5b23cedf7dd0a2035e886018e
Matt Eppy <102959719+Mattjeppy@users.noreply.github.com>
2025-08-14 17:18:26 +0100

    update auth endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  signup(authDto: AuthDto) {
+    return { message: 'User signed up successfully', user: authDto };
+  }
+}

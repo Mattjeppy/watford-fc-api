106c0dce0f81acd445c511b77e8fbf252e8ff6d8
Matt Eppy <102959719+Mattjeppy@users.noreply.github.com>
2025-08-14 17:51:10 +0100

    set up prisma service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
+import { PrismaService } from '../prisma/prisma.service'
 
 @Injectable()
 export class AuthService {
-  signup(authDto: AuthDto) {
-    return { message: 'User signed up successfully', user: authDto };
+  async signup(authDto: AuthDto) {
+    const user = await PrismaService.user.create({
+      data: {
+        email: authDto.email,
+        password: authDto.password,
+      },
+    });
+    return { message: 'User signed up successfully', user };
   }
 }

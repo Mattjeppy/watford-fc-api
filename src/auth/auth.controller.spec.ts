0b80114f46c48b444b47344f6ec0d5bdce07df77
Matt Eppy <102959719+Mattjeppy@users.noreply.github.com>
2025-08-15 17:52:05 +0100

    finish current testing for signup

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -4,13 +4,27 @@ import request from 'supertest';
 import { AppModule } from '../app.module';
 import { PrismaService } from '../prisma/prisma.service';
 
-describe('AuthController (e2e)', () => {
+// look into using a mock sqlite database
+
+describe('AuthController', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
+    const users: any[] = [];
     const prismaMock = {
-      user: { create: jest.fn(), findUnique: jest.fn() },
-    };
+  user: {
+    create: jest.fn(async ({ data }) => {
+      if (users.find(u => u.email === data.email)) {
+        const error: any = new Error('Unique constraint failed');
+        error.code = 'P2002'; // Prisma unique constraint code
+        throw error;
+      }
+      users.push(data);
+      return data;
+    }),
+    findUnique: jest.fn(({ where }) => users.find(u => u.email === where.email)),
+  },
+};
 
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -27,8 +41,8 @@ describe('AuthController (e2e)', () => {
   it('should fail if provided invalid email', async () => {
     await request(app.getHttpServer())
       .post('/auth/signup')
-      .send({ email: 'invalid', password: 'password' })
-      .expect(400);
+      .send({ email: 'invalid', password: 'StrongPassword1!' })
+      .expect(400, /email must be an email/);
   });
   it('should fail if provided password less than eight characters', async () => {
     await request(app.getHttpServer())
@@ -41,6 +55,25 @@ describe('AuthController (e2e)', () => {
       .post('/auth/signup')
       .send({ email: 'test@email.com', password: 'abc' })
       .expect(400, /password must be longer than or equal to 8 characters/);
+  });
+  it('should fail if provided password is too weak', async () => {
+    await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: 'test@email.com', password: 'weakpassword' })
+      .expect(400, /password is too weak/);
+  });
+  it('should reject duplicate emails', async () => {
+    const dto = { email: 'test@email.com', password: 'StrongPassword1!' }
+    // first sign-up attempt
+    await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send(dto)
+      .expect(201);
+    // second sign-up attempt (same credentials)
+    await request(app.getHttpServer())
+    .post('/auth/signup')
+    .send(dto)
+    .expect(500, /Internal server error/);
   });
 
   afterAll(async () => {

9e68eb01394a864fa0eaaae2755b0d95f0c15982
Matt Eppy <102959719+Mattjeppy@users.noreply.github.com>
2025-08-15 17:41:56 +0100

    update module imports

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,11 +6,11 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  async onModuleInit(): Promise<void> {
-    await (this.$connect as () => Promise<void>)();
+  async onModuleInit() {
+    await this.$connect();
   }
 
-  async onModuleDestroy(): Promise<void> {
-    await (this.$disconnect as () => Promise<void>)();
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }

8aa9f57b34031624ba5f977a58ab7a3752ea5b6a
Matt Eppy <102959719+Mattjeppy@users.noreply.github.com>
2025-08-18 13:29:24 +0100

    start implementation of JWT logic

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,27 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    ConfigModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,32 +1,38 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import * as argon2 from 'argon2';
-
+import { JwtService } from '@nestjs/jwt';
 import { AuthDto } from './dto/auth.dto';
-import { PrismaService } from '../prisma/prisma.service'
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService
+  ) {}
+
   async signup(authDto: AuthDto) {
     const hashedPw = await argon2.hash(authDto.password);
     const email = authDto.email.toLowerCase();
     const user = await this.prismaService.user.create({
-      data: {
-        email,
-        password: hashedPw,
-      },
+      data: { email, password: hashedPw },
     });
     return { message: 'User signed up successfully', user: user.email };
   }
 
   async signin(authDto: AuthDto) {
-    //TODO: set up JWT service
-    const user = await this.prismaService.user.findUnique({ where: { email: authDto.email.toLowerCase() } });
+    const user = await this.prismaService.user.findUnique({
+      where: { email: authDto.email.toLowerCase() },
+    });
     if (!user) throw new UnauthorizedException('Invalid credentials');
 
-    const verifiedUser = await argon2.verify(user.password, authDto.password);
-    if (!verifiedUser) throw new UnauthorizedException('Invalid credentials');
+    const verified = await argon2.verify(user.password, authDto.password);
+    if (!verified) throw new UnauthorizedException('Invalid credentials');
 
-    return { message: 'User signed in successfully', user: user.email };
+    const payload = { sub: user.id, email: user.email };
+    const token = this.jwtService.sign(payload);
+
+    return { access_token: token };
   }
 }
+
